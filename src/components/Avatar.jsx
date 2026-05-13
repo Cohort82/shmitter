e8bb860a51790c1e8d9a81b4dd1ae302abf1500a
edward <edwardlfish@gmail.com>
2026-05-13 12:27:39 +0300

    Refactor: Introduce ShmitterContext to manage user and stats globally

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,4 +1,9 @@
-const Avatar = ({user, size}) => {
+import {useContext} from "react";
+import {ShmitterContext} from "../utils/context.js";
+
+const Avatar = ({size}) => {
+    const {user} = useContext(ShmitterContext);
+
     return (
         <img className={`user-avatar ${size ?? ''}`} src={user.avatar} alt={user.name}/>
     )
