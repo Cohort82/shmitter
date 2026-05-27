cea1a37065fc53a86ea8168783c033e8c1f507c1
edward <edwardlfish@gmail.com>
2026-05-27 12:22:32 +0300

    Refactor reducers: Replace `shmitterReducer` with modular `rootReducer`, splitting logic into `userReducer` and `statsReducer`. Update store configuration accordingly.

---
+++ src/reducer/statsReducer.js
@@ -0,0 +1,19 @@
+import {CHANGE_STATS} from "../actions/statsActions.js";
+
+/*
+type State = {
+        followers: number,
+        following: string,
+    }
+ */
+
+export const statsReducer = (state, action) => {
+    switch (action.type) {
+        case CHANGE_STATS: {
+            const res = state[action.payload.statsType] + action.payload.sum;
+            return {...state, [action.payload.statsType]: res < 0 ? 0 : res};
+        }
+        default:
+            return state;
+    }
+}
